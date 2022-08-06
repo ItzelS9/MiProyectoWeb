2dcedc1a0e8f1b15cf7cbfcbf62c55d5e2a62d80
ItzelS9 <105961765+ItzelS9@users.noreply.github.com>
2022-08-06 01:03:33 -0500

    update

--- js/num_angelical.js
+++ js/num_angelical.js
@@ -19,16 +19,6 @@ var long=0;
             console.log(res);
             var imagen=document.getElementById('pkm-img');
             imagen.src=res[0].figure;
-            var p1=document.getElementById('name');
-            p1.innerHTML="<b>Nombre: </b>"+res[0].nombre;
-            var p2=document.getElementById('type');
-            p2.innerHTML="<b>Tipo: </b>"+res[0].tipo;
-            var p3=document.getElementById('height');
-            p3.innerHTML="<b>Altura: </b>"+res[0].altura+"cms.";
-            var p4=document.getElementById('weight');
-            p4.innerHTML="<b>Peso: </b>"+res[0].peso+"kg.";
-            var p5=document.getElementById('des');
-            p5.innerHTML="<b>Descripción: </b>"+res[0].descripcion;
         })
 
         .catch(function(error){
